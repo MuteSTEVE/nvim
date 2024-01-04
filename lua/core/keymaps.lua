9a5feda6de3d194b9175dc26cd5b7b3f92704ab4
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2024-01-04 15:09:44 +0700

    Adding some keymaps

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -3,21 +3,28 @@ vim.g.mapleader = ' '
 
 -- Define map as keybind
 local map = vim.api.nvim_set_keymap
-local nrs = { noremap = true, silent = false }
-local nr = { noremap = true, silent = true }
+local nr = { noremap = true, silent = false }
+local nrs = { noremap = true, silent = true }
+
+map('n', "<leader>w", "<cmd>w!<CR>", nr)
+map('n', "<S-q>", "<cmd>q!<CR>", nr)
+map('n', "<leader>h", "<cmd>noh<CR>", nrs)
+
+-- Make executable files
+map('x', "<leader>x", "<cmd>!chmod +x %<CR>", nrs)
 
 -- Lazy and Mason
-map("n", "<S-l>", "<cmd>Lazy<cr>", nr)
-map("n", "<S-m>", "<cmd>Mason<cr>", nr)
+map("n", "<S-l>", "<cmd>Lazy<cr>", nrs)
+map("n", "<S-m>", "<cmd>Mason<cr>", nrs)
 
 -- Some useful keybinds VSCode move line keybind
-map('n', 'K', "<cmd>move .-2<CR>==", nr)
-map('n', 'J', "<cmd>move .+1<CR>==", nr)
+map('n', 'K', "<cmd>move .-2<CR>==", nrs)
+map('n', 'J', "<cmd>move .+1<CR>==", nrs)
 
 -- buffer movement
-map('n', 'Z', '<cmd>bp<CR>', nr)
-map('n', 'C', '<cmd>bn<CR>', nr)
-map('n', 'X', '<cmd>bd<CR>', nr)
+map('n', 'Z', '<cmd>bp<CR>', nrs)
+map('n', 'C', '<cmd>bn<CR>', nrs)
+map('n', 'X', '<cmd>bd<CR>', nrs)
 
 -- Move between panes
 map('n', '<C-h>', '<C-w>h', nrs)
@@ -30,5 +37,5 @@ map('v', '<', '<gv', nrs)
 map('v', '>', '>gv', nrs)
 
 -- moving the cursor in insert mode
-map('i', '<C-h>', '<Left>', nr)
-map('i', '<C-l>', '<Right>', nr)
+map('i', '<C-h>', '<Left>', nrs)
+map('i', '<C-l>', '<Right>', nrs)
