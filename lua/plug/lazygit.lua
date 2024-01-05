8273eb4c2e6c96909b12aca11893337536294860
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2024-01-05 20:38:32 +0700

    Adding Lazygit plugin

---
+++ lua/plug/lazygit.lua
@@ -0,0 +1,10 @@
+local PLUG = {
+  "kdheepak/lazygit.nvim",
+  lazy = true,
+  cmd = "LazyGit",
+  keys = {
+    { "<leader>g", "<cmd>LazyGit<CR>" },
+  }
+}
+
+return PLUG
--- lua/plug/telescope.lua
+++ lua/plug/telescope.lua
@@ -4,10 +4,10 @@ local PLUG = {
   lazy = true,
   cmd = "Telescope",
   keys = {
-    { "<leader>f", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<cr>" },
-    { "<leader>H", "<cmd>Telescope oldfiles theme=ivy<cr>" },
-    { "<leader>b", "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<cr>" },
-    { "<C-f>", "<cmd>Telescope live_grep theme=ivy<cr>" }
+    { "<leader>f", "<cmd>lua require('telescope.builtin').find_files(require('telescope.themes').get_dropdown{previewer = false})<CR>" },
+    { "<leader>H", "<cmd>Telescope oldfiles theme=ivy<CR>" },
+    { "<leader>b", "<cmd>lua require('telescope.builtin').buffers(require('telescope.themes').get_dropdown{previewer = false})<CR>" },
+    { "<C-f>", "<cmd>Telescope live_grep theme=ivy<CR>" }
   }
 }
 
