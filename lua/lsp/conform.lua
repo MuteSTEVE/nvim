95104a63affbe6f4b72252587b773f75121e7ff4
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2024-02-15 09:07:58 +0700

    Some plugins adjustment

--- lua/lsp/conform.lua
+++ lua/lsp/conform.lua
@@ -1,24 +1,24 @@
 local PLUG = {
-  'stevearc/conform.nvim',
-  event = { "BufReadPost", "BufNewFile" },
+	"stevearc/conform.nvim",
+	event = { "BufReadPost", "BufNewFile" },
 }
 
 function PLUG.config()
-  local conform_ok, conform = pcall(require, "conform")
-  if not conform_ok then
-    return
-  end
+	local conform_ok, conform = pcall(require, "conform")
+	if not conform_ok then
+		return
+	end
 
-  conform.setup({
-    format_on_save = {
-      timeout_ms = 500,
-      lsp_fallback = true,
-    },
-    formatters_by_ft = {
-      lua = { "stylua" },
-      python = { "isort", "black" },
-    },
-  })
+	conform.setup({
+		format_on_save = {
+			timeout_ms = 100,
+			lsp_fallback = true,
+		},
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+		},
+	})
 end
 
 return PLUG
--- lua/plug/colorizer.lua
+++ lua/plug/colorizer.lua
@@ -1,16 +1,15 @@
 local PLUG = {
-  'NvChad/nvim-colorizer.lua',
-  ft = { "css", "js", "html" },
-  event = { "BufReadPost", "BufNewFile" }
+	"NvChad/nvim-colorizer.lua",
+	ft = { "css", "js", "html" },
 }
 
 function PLUG.config()
-  local colorizer_ok, colorizer = pcall(require, "colorizer")
-  if not colorizer_ok then
-    return
-  end
+	local colorizer_ok, colorizer = pcall(require, "colorizer")
+	if not colorizer_ok then
+		return
+	end
 
-  colorizer.setup({})
+	colorizer.setup({})
 end
 
 return PLUG
