8db5ec885c33185be28be5cb23c181ef960c8a06
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2024-02-19 10:12:59 +0700

    Nvim-colorizer to highlight color and hex color

---
+++ lua/plug/colorizer.lua
@@ -0,0 +1,15 @@
+local PLUG = {
+	"norcalli/nvim-colorizer.lua",
+	cmd = "ColorizerToggle",
+	ft = { "css", "js", "html" },
+}
+
+function PLUG.config()
+	local colorizer_ok, colorizer = pcall(require, "colorizer")
+	if not colorizer_ok then
+		return
+	end
+	colorizer.setup({})
+end
+
+return PLUG
