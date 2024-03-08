b46c1687d38f9aeb2297f75372b7c92def7e2dc6
MuteSTEVE <Dr.MuteSTEVE2@protonmail.com>
2024-03-08 13:32:25 +0700

    Positioning line location on statusline

--- lua/core/statusline.lua
+++ lua/core/statusline.lua
@@ -1,23 +1,24 @@
 local icons_ok, icons = pcall(require, "core.icons")
 if not icons_ok then
-  return
+	return
 end
 local GIT = icons.git
 
 local function shell(command)
-  local handle = io.popen(command)
-  local result = handle:read("*a")
-  local icon = GIT.branch
-  handle:close()
-  if string.len(result) > 0 then
-    return icon .. result .. " " .. " "
-  else
-    return ""
-  end
+	local handle = io.popen(command)
+	local result = handle:read("*a")
+	local icon = GIT.branch
+	handle:close()
+	if string.len(result) > 0 then
+		return icon .. result .. " " .. " "
+	else
+		return ""
+	end
 end
 
 local background = "%#background#"
 local spc = " "
+local tab = spc .. spc .. spc .. spc .. spc .. spc .. spc .. spc
 local align_right = "%="
 local slash = [[/]]
 
@@ -30,23 +31,25 @@ local linepost = "%c" .. ":" .. "%l"
 local total_lines = "%L"
 local percentage = "%p%%"
 
+-- vim.cmd("highlight background guibg=white guifg=black") -- If you want original like vim
 vim.cmd("highlight background guibg=guibg guifg=white")
 vim.opt.laststatus = 3
 vim.opt.statusline = background
-    .. spc
-    .. spc
-    .. branch
-    .. file_name
-    .. spc
-    .. modified
-    .. spc
-    .. read_only
-    .. align_right
-    .. spc
-    .. spc
-    .. linepost
-    .. slash
-    .. total_lines
-    .. spc
-    .. percentage
-    .. spc
+	.. spc
+	.. spc
+	.. branch
+	.. file_name
+	.. spc
+	.. modified
+	.. spc
+	.. read_only
+	.. spc
+	.. spc
+	.. align_right
+	.. linepost
+	.. slash
+	.. total_lines
+	.. tab
+	.. spc
+	.. percentage
+	.. spc
