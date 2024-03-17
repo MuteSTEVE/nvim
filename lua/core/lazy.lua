77a5f7c22192a9bbc6386660c596c791b7afa3b1
MuteSTEVE <Dr.MuteSTEVE2@protonmailcom>
2024-03-17 16:09:47 +0700

    Some leftover keymaps

--- lua/core/lazy.lua
+++ lua/core/lazy.lua
@@ -49,18 +49,27 @@ lazy.setup({
 local map = vim.keymap.set
 local nrs = { noremap = true, silent = true }
 map("n", "<A-l>", "<cmd>Lazy<CR>", nrs)
-map("n", "<leader>g", function()
-  require("lazy.util").float_term({ "lazygit" })
-end, nrs)
-map("n", "<leader>t", function()
-  require("lazy.util").float_term({ "zsh" })
-end, nrs)
-map("n", "<leader>ll", function()
-  require("lazy.util").float_term({ "lazygit", "log" })
-end, nrs)
-map("n", "<leader>r", function()
-  require("lazy.util").float_term({ "ranger" })
-end, nrs)
-map("n", "<leader>m", function()
-  require("lazy.util").float_term({ "ncmpcpp" })
-end, nrs)
+map("n", "<leader>g",
+  function()
+    require("lazy.util").float_term({ "lazygit" })
+  end)
+map("n", "<leader>lg",
+  function()
+    require("lazy.util").float_term({ "lazygit" })
+  end)
+map("n", "<leader>t",
+  function()
+    require("lazy.util").float_term({ "zsh" })
+  end)
+map("n", "<leader>ll",
+  function()
+    require("lazy.util").float_term({ "lazygit", "log" })
+  end)
+map("n", "<leader>r",
+  function()
+    require("lazy.util").float_term({ "ranger" })
+  end)
+map("n", "<leader>m",
+  function()
+    require("lazy.util").float_term({ "ncmpcpp" })
+  end)
