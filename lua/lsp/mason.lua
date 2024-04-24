c0dac1cc0b66c3809abc238576c8557e24b87cdc
MuteSTEVE <Dr.MuteSTEVE2@protonmail.com>
2024-04-24 14:30:36 +0700

    Mason configuration update (￣▽￣)

--- lua/lsp/mason.lua
+++ lua/lsp/mason.lua
@@ -3,7 +3,7 @@ return {
     "williamboman/mason.nvim",
     event = { "BufReadPost", "BufNewFile" },
     cmd = "Mason",
-    keys = {{ "<S-m>", "<cmd>Mason<CR>" }},
+    keys = {{ "<A-m>", "<cmd>Mason<CR>" }},
     config = function()
       local mason_ok, mason = pcall(require, "mason")
       if not mason_ok then
@@ -11,19 +11,18 @@ return {
       end
       mason.setup()
     end,
-    dependencies = {{
-      "williamboman/mason-lspconfig.nvim",
-      event = { "BufReadPost", "BufNewFile" },
-      config = function()
-        local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
-        if not mason_lspconfig_ok then
-          return
-        end
-        mason_lspconfig.setup({
-          ensure_installed = { "pyright", "vimls", "lua_ls", "marksman", "emmet_language_server", },
-        })
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    event = { "BufReadPost", "BufNewFile" },
+    config = function()
+      local mason_lspconfig_ok, mason_lspconfig = pcall(require, "mason-lspconfig")
+      if not mason_lspconfig_ok then
+        return
       end
-    },
-    }
-  }
+      mason_lspconfig.setup({
+        ensure_installed = { "pyright", "vimls", "lua_ls", "marksman", "emmet_language_server", },
+      })
+    end
+  },
 }
