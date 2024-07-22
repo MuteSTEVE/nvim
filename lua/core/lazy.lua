21d2c118aec545f805b32f25843ae30b0ed733ec
MuteSTEVE <Dr.MuteSTEVE2@protonmail.com>
2024-07-22 19:33:32 +0700

    core/ directory added

---
+++ lua/core/lazy.lua
@@ -0,0 +1,74 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+vim.g.mapleader = " "
+
+local lazy_ok, lazy = pcall(require, "lazy")
+if not lazy_ok then
+  return
+end
+
+lazy.setup({
+  spec = {
+    { import = "lsp" },
+    { import = "plug" },
+  },
+  performance = {
+    cache = { enabled = true },
+    reset_packpath = true, -- reset the package path to improve startup time
+    rtp = {
+      reset = true,        -- reset the runtime path to $VIMRUNTIME and your config directory
+      disabled_plugins = {
+        "editorconfig",
+        "gzip",
+        "man",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
+        "nvim",
+        "rplugin",
+        "spellfile",
+        "tarPlugin",
+        "tohtml",
+        "tutor",
+        "zipPlugin",
+      },
+    },
+  },
+})
+
+local map = vim.keymap.set
+local float_term_ok, ft = pcall(require, "lazy.util")
+local float_term = ft.float_term
+if not float_term_ok then
+  return
+end
+
+map("n", "<A-l>", "<cmd>Lazy<CR>")
+map("n", "<leader>g", function()
+    float_term("lazygit")
+  end)
+map("n", "<leader>lg", function()
+    float_term("lazygit")
+  end)
+map("n", "<leader>ll", function()
+    float_term({ "lazygit", "log" })
+  end)
+map("n", "<leader>r", function()
+    float_term("ranger")
+  end)
+map("n", "<leader>m", function()
+    float_term("ncmpcpp")
+  end)
+map("n", "<leader>t", function()
+    float_term("bash")
+  end)
---
+++ lua/core/options.lua
@@ -0,0 +1,52 @@
+local options = {
+  backup = false,
+  cmdheight = 1,
+  completeopt = { "menuone", "noselect" },
+  conceallevel = 1,
+  expandtab = true,
+  fileencoding = "utf-8",
+  filetype = on,
+  guifont = "monospace:h17",
+  hlsearch = true,
+  ignorecase = true,
+  linebreak = true,
+  mouse = "a",
+  number = true,
+  numberwidth = 4,
+  pumheight = 10,
+  relativenumber = true,
+  scrolloff = 10,
+  shiftwidth = 2,
+  showmode = true,
+  sidescrolloff = 10,
+  signcolumn = "yes",
+  smartcase = true,
+  smartindent = true,
+  spelllang = "en_us",
+  spell = true,
+  splitbelow = true,
+  splitright = true,
+  swapfile = false,
+  syntax = on,
+  tabstop = 2,
+  termguicolors = true,
+  timeoutlen = 300,
+  undofile = true,
+  updatetime = 300,
+  whichwrap = "bs<>[]hl",
+  wrap = true,
+  writebackup = false,
+}
+
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
+
+vim.opt.shortmess:append("c")
+vim.opt.iskeyword:append("-")
+vim.opt.formatoptions:remove({ "c", "r", "o" })
+vim.opt.runtimepath:remove("/usr/share/vim/vimfiles")
+
+if not vim.fn.exists("guin running") then
+  vim.o.t_Co = 256
+end
