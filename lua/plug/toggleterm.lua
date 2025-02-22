1c0a61ed125c880bc1da1f676a73a349836a3d60
MuteSTEVE <Dr.MuteSTEVE2@protonmail.com>
2025-02-22 17:01:45 +0700

    Updated Toggleterm plugin keymap

--- lua/plug/toggleterm.lua
+++ lua/plug/toggleterm.lua
@@ -1,23 +1,23 @@
-local KEYMAP = {
+local MAP = {
 	lazygit = "<leader>lg",
 	ncmpcpp = "<leader>m",
 	ranger = "<leader>r",
-	term_tab = "<leader>tt",
-	term_float = "<leader>tf",
-	term_vertical = "<leader>tv",
-	term_horizontal = "<leader>th",
+	tab = "<leader>tt",
+	float = "<leader>tf",
+	vertical = "<leader>tv",
+	horizontal = "<leader>th",
 }
 local PLUG = {
 	"akinsho/toggleterm.nvim",
 	cmd = "ToggleTerm",
 	keys = {
-		{ KEYMAP.lazygit, "<cmd>lua Lazygit()<CR>" },
-		{ KEYMAP.ncmpcpp, "<cmd>lua Ncmpcpp()<CR>" },
-		{ KEYMAP.ranger, "<cmd>lua Ranger()<CR>" },
-		{ KEYMAP.term_tab, "<cmd>lua Term_tab()<CR>" },
-		{ KEYMAP.term_float, "<cmd>lua Term_float()<CR>" },
-		{ KEYMAP.term_vertical, "<cmd>lua Term_vertical()<CR>" },
-		{ KEYMAP.term_horizontal, "<cmd>lua Term_horizontal()<CR>" },
+		{ MAP.lazygit, "<cmd>lua Lazygit()<CR>" },
+		{ MAP.ncmpcpp, "<cmd>lua Ncmpcpp()<CR>" },
+		{ MAP.ranger, "<cmd>lua Ranger()<CR>" },
+		{ MAP.tab, "<cmd>ToggleTerm direction=tab<CR>" },
+		{ MAP.float, "<cmd>ToggleTerm direction=float<CR>" },
+		{ MAP.vertical, "<cmd>ToggleTerm direction=vertical size=50<CR>" },
+		{ MAP.horizontal, "<cmd>ToggleTerm direction=horizontal size=10<CR>" },
 	},
 }
 
@@ -36,49 +36,36 @@ function PLUG.config()
 	})
 
 	-- Create a custom toggleterm window
-	local ncmpcpp = Terminal:new({ cmd = "ncmpcpp", hidden = true })
+	local ncmpcpp = Terminal:new({ cmd = "ncmpcpp" })
 	function Ncmpcpp()
 		ncmpcpp:toggle()
 	end
-	local ranger = Terminal:new({ cmd = "ranger", hidden = true })
+	local ranger = Terminal:new({ cmd = "ranger" })
 	function Ranger()
 		ranger:toggle()
 	end
-	local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+	local lazygit = Terminal:new({ cmd = "lazygit" })
 	function Lazygit()
 		lazygit:toggle()
 	end
 
-	local term_tab = Terminal:new({ cmd = "bash", direction = "tab", hidden = true })
-	function Term_tab()
-		term_tab:toggle()
-	end
-	local term_float = Terminal:new({ cmd = "bash", direction = "float", hidden = true })
-	function Term_float()
-		term_float:toggle()
-	end
-	local term_vertical = Terminal:new({ cmd = "bash", direction = "vertical", hidden = true })
-	function Term_vertical()
-		term_vertical:toggle()
-	end
-	local term_horizontal = Terminal:new({ cmd = "bash", direction = "horizontal", hidden = true })
-	function Term_horizontal()
-		term_horizontal:toggle()
-	end
-
 	-- Escape terminal mode
 	local map = vim.keymap.set
-	local NR = { noremap = true, silent = true }
-	map("t", KEYMAP.lazygit, "<C-\\><C-n><cmd>q!<CR>", NR)
-	map("t", KEYMAP.ncmpcpp, "<C-\\><C-n><cmd>q!<CR>", NR)
-	map("t", KEYMAP.ranger, "<C-\\><C-n><cmd>q!<CR>", NR)
+	local NRS = { noremap = true, silent = true }
+	map("t", MAP.lazygit, "<C-\\><C-n><cmd>q!<CR>", NRS)
+	map("t", MAP.ncmpcpp, "<C-\\><C-n><cmd>q!<CR>", NRS)
+	map("t", MAP.ranger, "<C-\\><C-n><cmd>q!<CR>", NRS)
+	map("t", MAP.tab, "<C-\\><C-n><cmd>q!<CR>", NRS)
+	map("t", MAP.float, "<C-\\><C-n><cmd>q!<CR>", NRS)
+	map("t", MAP.vertical, "<C-\\><C-n><cmd>q!<CR>", NRS)
+	map("t", MAP.horizontal, "<C-\\><C-n><cmd>q!<CR>", NRS)
 
 	-- Move from terminal back and forth
-	map("t", "<C-h>", "<C-\\><C-n><C-w>h<CR>{}k$", NR)
-	map("t", "<C-j>", "<C-\\><C-n><C-w>j<CR>{}k$", NR)
-	map("t", "<C-k>", "<C-\\><C-n><C-w>k<CR>{}k$", NR)
-	map("t", "<C-l>", "<C-\\><C-n><C-w>l<CR>{}k$", NR)
-	map("t", "<S-Esc>", "<C-\\><C-n><CR>", NR)
+	map("t", "<C-h>", "<C-\\><C-n><C-w>h<CR>{}k$", NRS)
+	map("t", "<C-j>", "<C-\\><C-n><C-w>j<CR>{}k$", NRS)
+	map("t", "<C-k>", "<C-\\><C-n><C-w>k<CR>{}k$", NRS)
+	map("t", "<C-l>", "<C-\\><C-n><C-w>l<CR>{}k$", NRS)
+	map("t", "<S-Esc>", "<C-\\><C-n><CR>", NRS)
 end
 
 return PLUG
