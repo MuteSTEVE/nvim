783a320d9b044b2870a0597fcf66fef6383fc07d
MuteSTEVE <Dr.MuteSTEVE2@protonmail.com>
2025-03-06 11:52:04 +0700

    LSP

--- lua/plug/treesitter.lua
+++ lua/plug/treesitter.lua
@@ -12,7 +12,7 @@ function PLUG.config()
 
   treesitter.setup({
     auto_install = true,
-    ensure_installed = { "bash", "lua", "vim", "python", "markdown", "markdown_inline" },
+    ensure_installed = { "c", "cpp", "bash", "lua", "vim", "python", "markdown", "markdown_inline" },
     sync_install = true,
     highlight = {
       enable = true,
