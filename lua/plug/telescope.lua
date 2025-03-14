582881fd10fe2c3630186a2244a8682ac1b40848
MuteSTEVE <Dr.MuteSTEVE2@protonmail.com>
2025-03-14 08:52:32 +0700

    Removing webdevicons dependencies from telescope plugin

--- lua/plug/telescope.lua
+++ lua/plug/telescope.lua
@@ -1,35 +1,32 @@
 local PLUG = {
   "nvim-telescope/telescope.nvim",
-  dependencies = {
-    "nvim-lua/plenary.nvim",
-    -- "kyazdani42/nvim-web-devicons",
-  },
+  dependencies =  "nvim-lua/plenary.nvim",
   cmd = "Telescope",
   keys = {
     { "<leader>f", function()
-        require("telescope.builtin").find_files(
-          require("telescope.themes").get_dropdown({ previewer = false }))
-      end
+      require("telescope.builtin").find_files(
+        require("telescope.themes").get_dropdown({ previewer = false }))
+    end
     },
     { "<leader>F", function()
-        require("telescope.builtin").find_files(
-          require("telescope.themes").get_dropdown({ previewer = false, hidden = true }))
-      end
+      require("telescope.builtin").find_files(
+        require("telescope.themes").get_dropdown({ previewer = false, hidden = true }))
+    end
     },
     { "<leader>o", function()
-        require("telescope.builtin").oldfiles(
-          require("telescope.themes").get_ivy({ previewer = true }))
-      end
+      require("telescope.builtin").oldfiles(
+        require("telescope.themes").get_ivy({ previewer = true }))
+    end
     },
     { "<leader>b", function()
-        require("telescope.builtin").buffers(
-          require("telescope.themes").get_dropdown({ previewer = false }))
-      end
+      require("telescope.builtin").buffers(
+        require("telescope.themes").get_dropdown({ previewer = false }))
+    end
     },
     { "<C-f>", function()
-        require("telescope.builtin").live_grep(
-          require("telescope.themes").get_ivy({ previewer = true }))
-      end
+      require("telescope.builtin").live_grep(
+        require("telescope.themes").get_ivy({ previewer = true }))
+    end
     },
   },
 }
@@ -47,12 +44,12 @@ function PLUG.config()
   if not icons_ok then
     return
   end
-  local TL = icons.telescope
+  local IT = icons.telescope
 
   telescope.setup({
     defaults = {
-      prompt_prefix = " " .. TL.find .. " " .. " ",
-      selection_caret = TL.select .. " ",
+      prompt_prefix = " " .. IT.find .. " " .. " ",
+      selection_caret = IT.select .. " ",
       mappings = {
         i = {
           ["<C-j>"] = actions.move_selection_next,
